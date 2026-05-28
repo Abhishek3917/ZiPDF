e9f8c3845fe9dc3852f40a60b9facba63621efe0
Abhishek <abhishekskumar709@gmail.com>
2026-05-28 20:56:05 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,21 @@
+
+import {Header} from './components/Header'
+import { Body } from './components/Body'
+import { ToolsGrid } from './components/ToolsGrid'
+import { Footer } from './components/Footer'
+import { CopyRight } from './components/CopyRight'
+
+function App() {
+    return(
+      <>   
+            <Header />
+            <Body /> 
+            <ToolsGrid />
+            <Footer />
+            <CopyRight/>
+                  
+      </>
+    )
+}
+
+export default App
